e5f48d823c088145e1796a1bfeecbb282477a206
Allen Mathews <allenjoemathews@gmail.com>
2021-11-23 07:01:07 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+namesArr = ["Ada", "Brendan", "Ali"]
+
+
+function writeCards(namesArr, event) {
+    let cards = [];
+    for (let i = 0; i < namesArr.length; i++) {
+        cards.push(`Thank you, ${namesArr[i]}, for the wonderful surprise gift!`)
+    }
+    return cards;
+}
+
+
+function countDown(startingNum) {
+    while (startingNum > 0) {
+        console.log(startingNum);
+        startingNum -= 1;
+    }
+    console.log(startingNum)
+}
